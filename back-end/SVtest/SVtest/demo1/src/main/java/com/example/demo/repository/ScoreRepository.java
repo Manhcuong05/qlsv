8b6000ec2a2af40cebca44d6d118dd2bb941a833
Manhcuong05 <ngmanhcuong2011@gmail.com>
2025-11-03 10:01:59 +0700

    delete BE and change FE

--- back-end/SVtest/SVtest/demo1/src/main/java/com/example/demo/repository/ScoreRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.Score;
-
-public interface ScoreRepository extends JpaRepository<Score, Long> {
-}
